bbfa028c4a7b9192cd43e5af336d9eb711609a57
im-neiru <cantorne.neil@gmail.com>
2025-03-11 07:01:35 +0800

    fix: panic any on main

--- crates/vector/src/main.rs
+++ crates/vector/src/main.rs
@@ -1,4 +1,14 @@
+use std::panic::panic_any;
+
 fn main() {
     logging::set_panic_hook();
-    ui::App::new().unwrap().run().unwrap();
+
+    match ui::App::new() {
+        Ok(app) => {
+            if let Err(err) = app.run() {
+                panic_any(err)
+            }
+        }
+        Err(err) => panic_any(err),
+    }
 }
