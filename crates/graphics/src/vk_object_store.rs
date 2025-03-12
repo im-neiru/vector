ab8d8950b6e4c6e04527b83af60d721c5be70bc6
im-neiru <cantorne.neil@gmail.com>
2025-03-13 07:21:08 +0800

    feat: add use_object method for safe object retrieval in VkObjectStore

--- crates/graphics/src/vk_object_store.rs
+++ crates/graphics/src/vk_object_store.rs
@@ -24,6 +24,38 @@ where
     K: PartialEq + PartialOrd + Ord + Eq,
 {
     #[inline]
+    pub(crate) unsafe fn use_object<
+        F: FnOnce() -> logging::Result<T>,
+    >(
+        &mut self,
+        key: K,
+        constructor: F,
+    ) -> logging::Result<&T> {
+        let storage = self.0.get_or_insert(Vec::new());
+
+        let index = match storage
+            .binary_search_by(|entry| entry.key.cmp(&key))
+        {
+            Ok(index) => index,
+            Err(dest) => {
+                let object = constructor()?;
+
+                storage.insert(dest, Entry { key, object });
+
+                dest
+            }
+        };
+
+        Ok(unsafe {
+            &storage
+                .get(index)
+                .as_ref()
+                .unwrap_unchecked()
+                .object
+        })
+    }
+
+    #[inline]
     pub(crate) unsafe fn destroy<F: Fn(T)>(
         &mut self,
         destructor: F,
