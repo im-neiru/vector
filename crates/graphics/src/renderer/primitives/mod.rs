1fe930110448e0ad606438fe1c2d1b97f5d15114
im-neiru <cantorne.neil@gmail.com>
2025-03-13 07:04:30 +0800

    feat: remove unused renderer components and traits for cleaner codebase

--- crates/graphics/src/renderer/primitives/mod.rs
+++
@@ -1,5 +0,0 @@
-mod primitive;
-mod rounded_rectangle;
-
-pub use primitive::{Primitive, PrimitiveState};
-pub use rounded_rectangle::*;
--- crates/graphics/src/renderer/uniforms/uniform_trait.rs
+++
@@ -1,3 +0,0 @@
-pub(in crate::renderer) trait UniformTrait {
-    fn binding_type() -> wgpu::BindingType;
-}
