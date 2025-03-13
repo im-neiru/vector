94bd2a231acea913ec36b565922c18030ed34c78
im-neiru <cantorne.neil@gmail.com>
2025-03-13 18:54:19 +0800

    feat: implement shader usage methods in VertexShaderStore and FragmentShaderStore

--- crates/graphics/src/vk_object_store/shader.rs
+++ crates/graphics/src/vk_object_store/shader.rs
@@ -1,4 +1,4 @@
-use ash::vk::ShaderModule;
+use ash::vk::{self, ShaderModule, TaggedStructure};
 
 use crate::{
     allocation_callbacks::ALLOCATION_CALLBACKS,
@@ -14,6 +14,8 @@ pub(crate) type FragmentShaderStore = super::VkObjectStore<
     crate::spirv::fs::FragmentShaderId,
     ShaderModule,
 >;
+
+const ENTRY_POINT_NAME: *const i8 = c"main".as_ptr();
 
 impl<K> super::VkObjectStore<K, ShaderModule>
 where
@@ -61,5 +63,99 @@ where
         storage.sort_by(|a, b| a.key.cmp(&b.key));
 
         Ok(Self(Some(storage)))
+    }
+
+    fn internal_use_shader(
+        &mut self,
+        device: &ash::Device,
+        source: &ShaderSource<K>,
+    ) -> logging::Result<&vk::ShaderModule> {
+        let storage = self.0.get_or_insert(Vec::new());
+
+        let index = match storage
+            .binary_search_by(|entry| entry.key.cmp(&source.id))
+        {
+            Ok(index) => index,
+            Err(dest) => {
+                let module = unsafe {
+                    device
+                        .create_shader_module(
+                            &source.create_info(),
+                            ALLOCATION_CALLBACKS,
+                        )
+                        .map_err(|err| {
+                            logging::ErrorKind::VulkanError {
+                                function_name: "create_shader_module",
+                                vk_code: err.as_raw(),
+                            }
+                            .into_error()
+                        })?
+                };
+
+                storage.insert(
+                    dest,
+                    super::Entry {
+                        key: source.id,
+                        object: module,
+                    },
+                );
+
+                dest
+            }
+        };
+
+        Ok(unsafe {
+            &storage
+                .get(index)
+                .as_ref()
+                .unwrap_unchecked()
+                .object
+        })
+    }
+}
+
+impl VertexShaderStore {
+    #[inline]
+    pub(crate) fn use_shader(
+        &mut self,
+        device: &ash::Device,
+        source: &ShaderSource<crate::spirv::vs::VertexShaderId>,
+    ) -> logging::Result<vk::PipelineShaderStageCreateInfo>
+    {
+        let module =
+            self.internal_use_shader(device, source)?;
+
+        Ok(vk::PipelineShaderStageCreateInfo {
+            s_type: vk::PipelineShaderStageCreateInfo::STRUCTURE_TYPE,
+            p_next: core::ptr::null(),
+            stage: vk::ShaderStageFlags::VERTEX,
+            module: *module,
+            p_name: ENTRY_POINT_NAME,
+            ..Default::default()
+        })
+    }
+}
+
+impl FragmentShaderStore {
+    #[inline]
+    pub(crate) fn use_shader(
+        &mut self,
+        device: &ash::Device,
+        source: &ShaderSource<
+            crate::spirv::fs::FragmentShaderId,
+        >,
+    ) -> logging::Result<vk::PipelineShaderStageCreateInfo>
+    {
+        let module =
+            self.internal_use_shader(device, source)?;
+
+        Ok(vk::PipelineShaderStageCreateInfo {
+            s_type: vk::PipelineShaderStageCreateInfo::STRUCTURE_TYPE,
+            p_next: core::ptr::null(),
+            stage: vk::ShaderStageFlags::FRAGMENT,
+            module: *module,
+            p_name: ENTRY_POINT_NAME,
+            ..Default::default()
+        })
     }
 }
