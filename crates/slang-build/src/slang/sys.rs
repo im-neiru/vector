29f1165cb0ffec1577dc09c5a864cbab6839af4e
im-neiru <cantorne.neil@gmail.com>
2025-03-21 01:24:22 +0800

    feat: remove deprecated sys.rs and restructure module organization

--- crates/slang-build/src/slang/sys.rs
+++
@@ -1,8 +0,0 @@
-use std::ptr::NonNull;
-
-#[link(name = "slang")]
-unsafe extern "C" {
-
-    #[link_name = "slang_createGlobalSession"]
-    pub(super) fn slang_create_global_session(version: i32);
-}
---
+++ crates/slang-build/src/slang/sys/mod.rs
@@ -0,0 +1,10 @@
+mod bindings;
+mod global_session;
+mod slang_global_session_desc;
+mod slang_result;
+
+pub(crate) use global_session::IGlobalSession;
+pub(crate) use slang_global_session_desc::SlangGlobalSessionDesc;
+pub(crate) use slang_result::SlangResult;
+
+pub use bindings::*;
